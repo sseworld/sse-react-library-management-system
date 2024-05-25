b3551207453fd8fb5fd9d11e81f6adac9648aa10
sseworld <suryakantmaurya2007@gmail.com>
2024-05-25 19:54:11 +0530

    Setup Protected Route

---
+++ frontend/src/components/access-control/admin-protector.jsx
@@ -0,0 +1,12 @@
+import { Navigate } from "react-router-dom";
+import { useUser } from "../../context/user-context"
+
+export const WithAdminProtector = ({ children }) => {
+    const { isAdmin } = useUser()
+
+    if(isAdmin) {
+        return children
+    }
+
+    return <Navigate to="/" replace />
+}
---
+++ frontend/src/components/access-control/login-protector.jsx
@@ -0,0 +1,15 @@
+import { NotificationManager } from "react-notifications"
+import { Navigate } from "react-router-dom"
+import { useUser } from "../../context/user-context"
+
+export const WithLoginProtector = ({ children }) => {
+    const { user } = useUser()
+
+    if(user) {
+        return children
+    } else {
+        NotificationManager.error('Please login to proceed!')
+    }
+
+    return <Navigate to="/" replace />
+}
