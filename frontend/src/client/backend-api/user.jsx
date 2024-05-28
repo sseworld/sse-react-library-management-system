d58964bae48347086e0f64dbbff2394f6693e9dd
sseworld <suryakantmaurya2007@gmail.com>
2024-05-28 06:55:30 +0530

    Fixxing error Part - 2

--- frontend/src/client/backend-api/user.jsx
+++ frontend/src/client/backend-api/user.jsx
@@ -1,6 +1,6 @@
 export const UserApi = {
   borrowBook: async (isbn, userId) => {
-    const res = await fetch(`${Api}/v1/user/borrow`, {
+    const res = await fetch(`https://sse-react-library-management-system.onrender.com/v1/user/borrow`, {
       method: "POST",
       body: JSON.stringify({ isbn, userId }),
       headers: { "Content-Type": "application/json" },
@@ -8,7 +8,7 @@ export const UserApi = {
     return res.json();
   },
   returnBook: async (isbn, userId) => {
-    const res = await fetch(`${Api}/v1/user/return`, {
+    const res = await fetch(`https://sse-react-library-management-system.onrender.com/v1/user/return`, {
       method: "POST",
       body: JSON.stringify({ isbn, userId }),
       headers: { "Content-Type": "application/json" },
@@ -16,11 +16,11 @@ export const UserApi = {
     return res.json();
   },
   getBorrowBook: async () => {
-    const res = await fetch(`${Api}/v1/user/borrowed-books`, { method: "GET" });
+    const res = await fetch(`https://sse-react-library-management-system.onrender.com/v1/user/borrowed-books`, { method: "GET" });
     return res.json();
   },
   login: async (username, password) => {
-    const res = await fetch(`${Api}/v1/user/login`, {
+    const res = await fetch(`https://sse-react-library-management-system.onrender.com/v1/user/login`, {
       method: "POST",
       body: JSON.stringify({ username, password }),
       headers: { "Content-Type": "application/json" },
@@ -28,11 +28,11 @@ export const UserApi = {
     return res.json();
   },
   getProfile: async () => {
-    const res = await fetch(`${Api}/v1/user/profile`, { method: "GET" });
+    const res = await fetch(`https://sse-react-library-management-system.onrender.com/v1/user/profile`, { method: "GET" });
     return res.json();
   },
   logout: async () => {
-    const res = await fetch(`${Api}/v1/user/logout`, { method: "GET" });
+    const res = await fetch(`https://sse-react-library-management-system.onrender.com/v1/user/logout`, { method: "GET" });
     return res.json();
   },
 };
